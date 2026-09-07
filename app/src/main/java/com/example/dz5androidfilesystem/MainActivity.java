cefa5ee5c32281c2a1af27b70d896eba20d0e64f
1508opossum1987 <1508opossum1987@gmail.com>
2026-09-07 22:08:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dz5androidfilesystem/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.dz5androidfilesystem;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void isVegitarianCheckBoxOnClicked(View view){
+
+    }
+
+    public void saveButtonOnClick(View view){
+
+    }
+
+    public void loadButtonOnClick(View view){
+
+    }
+}
